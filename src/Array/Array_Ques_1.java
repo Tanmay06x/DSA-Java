6863871aba38002634a1326f91892a0a70fa8984
Tanmay <tanmaymeghwani990@gmail.com>
2026-09-21 13:36:40 +0530

    Organized DSA topics

---
+++ src/Array/Array_Ques_1.java
@@ -0,0 +1,19 @@
+package Array;
+
+public class Array_Ques_1 {
+    public static void main(String[] args) {
+//      #1 Two Sum Problem (Easy)
+        int[] nums={2,11,7,5};
+        int target=6;
+
+        for(int i=0;i<nums.length;i++){
+            int n1=nums[i];
+            for(int j=i+1;j<nums.length;j++){
+                if(n1+nums[j]==target){
+                    System.out.println(i+","+j);
+                    return;
+                }
+            }
+        }
+    }
+}
